32f0e4a65876b498399f9125319a96b8c3daae42
diegoPaladino <diegopaladinofoto@gmail.com>
2020-09-03 17:51:20 -0300

    2009031751

---
+++ proxy_anonimous_bot.py
@@ -0,0 +1,19 @@
+#proxy_anonimous_bot
+#not sucessful
+#link of hide my name = https://hidemy.name/en/proxy-list/?type=s#list
+#link of ip check: https://www.myip.com
+
+
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+PROXY = '169.57.157.148:80'
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument(f'--proxy-server={PROXY}')
+
+driver = webdriver.Chrome(
+    ChromeDriverManager().install(), options=chrome_options)
+driver.get('https://https://www.myip.com/')
+time.sleep(100)
